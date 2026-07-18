07503f8b5ff00b5ffa319a2b6a01a2c49649a701
digital-invita <anuar.rios.310@gmail.com>
2026-07-18 16:08:14 -0600

    Clear eventLocation field in app.js
    Updated eventLocation to an empty string.

--- assets/app.js
+++ assets/app.js
@@ -24,7 +24,7 @@ const INVITATION_CONFIG =
     "eventDate": "2027-03-11T15:33",
     "eventEnd": "2027-03-11T23:11",
     "timeZone": "America/Mexico_City",
-    "eventLocation": "Top Salón Obispado",
+    "eventLocation": "",
     "rsvpPhone": "5550591264"
   },
   "theme": {
